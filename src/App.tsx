ab71c0fedc8bc52068d56c02d053848216f0af5b
Antonije Ljubisa <ogistdipen@outlook.com>
2024-12-02 00:27:45 +0100

    testing db

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+import { getAllUsers } from "./api/users/getAllUsers";
+
 const App = () => {
+  const [users, setUsers] = useState([]);
+  
+  useEffect(() => {
+    getAllUsers().then((response) => {
+      setUsers(response);
+    });
+  }, []);
+
   return (
     <>
       Hello G! Here is Luffy coming strong! :D
+      <ul>
+        {users.map((user: any) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </>
   )
 }

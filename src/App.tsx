9458048a2887e69034d17ef097241f42e6c831ea
Antonije Ljubisa <ogistdipen@outlook.com>
2024-12-07 15:19:29 +0100

    updated hp

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,8 @@
-import { useEffect, useState } from "react";
-import { getAllUsers } from "./api/users/getAllUsers";
-
 const App = () => {
-  const [users, setUsers] = useState([]);
-  
-  useEffect(() => {
-    getAllUsers().then((response) => {
-      setUsers(response);
-    });
-  }, []);
 
   return (
     <>
-      Hello G! Here is Luffy coming strong! :D
-      <ul>
-        {users.map((user: any) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      A Lj - Portfolio
     </>
   )
 }
--- src/api/users/getAllUsers.ts
+++
@@ -1,4 +0,0 @@
-export const getAllUsers = async () => {
-  const response = await fetch('http://localhost:3000/users')
-  return await response.json()
-}
